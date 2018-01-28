5811e90d50ae9b04e4400b4e5b246da7b0de2bd8
beb97 <beb97@hotmail.com>
2018-01-28 21:27:34 +0100

    MaJ recherche

--- messaging/views.py
+++ messaging/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic.detail import DetailView
 
 from .models import Question
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -18,15 +20,17 @@ def detail(request, question_id):
 
 
 def question(request):
-    Question.objects.create(content=request.POST.get('question'),
-                            title=request.POST.get('title'))
-    return redirect('messaging:index')
+    created_question = Question.objects.create(content=request.POST.get('question'),
+                                               title=request.POST.get('title'),
+                                               author=request.user)
+    return redirect('messaging:detail', question_id=created_question.id)
 
 
 @login_required
 def answer(request, question_id):
     answered_question = get_object_or_404(Question, pk=question_id)
-    answered_question.answers.create_message(request.POST.get('answer'))
+    answered_question.answers.create(content=request.POST.get('answer'),
+                                     author=request.user)
     return redirect('messaging:detail', question_id=question_id)
 
 
@@ -34,8 +38,18 @@ def search(request):
     search_term = request.GET.get('search')
     questions_list = []
     if search_term:
-        questions_list = Question.objects.filter(title__contains=search_term).order_by('creation_date')[:5]
+        questions_list = Question.objects.get_answered_questions().filter(
+            title__contains=search_term).order_by('creation_date')[:5]
+
     context = {
         'questions_list': questions_list,
     }
     return render(request, 'messaging/search.html', context)
+
+
+class UserDetailView(DetailView):
+    model = User
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
